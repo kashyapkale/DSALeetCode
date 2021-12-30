8b9b404ce944a12f3250385c75d48cd2793590f8
Kashyap Milind Kale <43263855+kashyapkale@users.noreply.github.com>
2021-12-31 00:20:12 +0530

    Create minCostClimbingStairs.cpp

---
+++ Dynamic Programming Study Plan List/minCostClimbingStairs.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/min-cost-climbing-stairs/submissions/
+
+int minCostClimbingStairs(vector<int>& cost) {
+    int n = cost.size();
+
+    if (n == 1) {
+        return cost[0];
+    }
+    if (n == 2) {
+        return min(cost[0], cost[1]);
+    }
+    int dp[n + 1];
+
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    for (int i = 2; i < n; i++) {
+        dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+    }
+    return min(dp[n - 2], dp[n - 1]);
+}
